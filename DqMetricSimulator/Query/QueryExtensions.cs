641d7722dde0c52f8d36a7d70edbc6219e5a61cd
naiemk@gmail.com <naiemk@gmail.com@4b432d9e-e227-a383-5a4a-3dc17a29ecdf>
2011-05-16 10:56:23 +0000

    Addedooli

--- DqMetricSimulator/Query/QueryExtensions.cs
+++ DqMetricSimulator/Query/QueryExtensions.cs
@@ -34,27 +34,27 @@ namespace DqMetricSimulator.Query
             switch (op1)
             {
                 case ExpressionType.Equal:
-                    return var1 == var2 && con1 == con2;
+                    return (var1 == var2 && con1 == con2) ||
+                           (var1 == var2 && op2 == ExpressionType.GreaterThan && ConstantIsGt(con1, con2)) ||
+                           (var1 == var2 && op2 == ExpressionType.GreaterThanOrEqual && ConstantIsGte(con1, con2)) ||
+                           (var1 == var2 && op2 == ExpressionType.LessThan && ConstantIsGt(con2, con1)) ||
+                           (var1 == var2 && op2 == ExpressionType.LessThanOrEqual && ConstantIsGt(con2, con1));
                 case ExpressionType.GreaterThanOrEqual:
-                    return var1 == var2 &&
-                           (op1 == op2) &&
-                           con1 is IComparable &&
-                           (((IComparable)con1).CompareTo(con2) >= 1);
+                    return //(var1 == var2 && op2 == ExpressionType.Equal && ConstantIsGte(con1, con2)) ||
+                           (var1 == var2 && op2 == ExpressionType.GreaterThan && ConstantIsGt(con1, con2)) ||
+                           (var1 == var2 && op2 == ExpressionType.GreaterThanOrEqual && ConstantIsGte(con1, con2));
                 case ExpressionType.LessThanOrEqual:
-                    return var1 == var2 &&
-                           (op1 == op2) &&
-                           con1 is IComparable &&
-                           (((IComparable)con1).CompareTo(con2) <= 1);
+                    return //(var1 == var2 && op2 == ExpressionType.Equal && ConstantIsGte(con2, con1)) ||
+                           (var1 == var2 && op2 == ExpressionType.LessThan && ConstantIsGt(con2, con1)) ||
+                           (var1 == var2 && op2 == ExpressionType.LessThanOrEqual && ConstantIsGte(con2, con1));
                 case ExpressionType.GreaterThan:
-                    return var1 == var2 &&
-                           (op1 == op2) &&
-                           con1 is IComparable &&
-                           (((IComparable)con1).CompareTo(con2) > 1);
+                    return //(var1 == var2 && op2 == ExpressionType.Equal && ConstantIsGt(con1, con2)) ||
+                           (var1 == var2 && op2 == ExpressionType.GreaterThan && ConstantIsGte(con1, con2)) ||
+                           (var1 == var2 && op2 == ExpressionType.GreaterThanOrEqual && ConstantIsGte(con1, con2));
                 case ExpressionType.LessThan:
-                    return var1 == var2 &&
-                           (op1 == op2) &&
-                           con1 is IComparable &&
-                           (((IComparable)con1).CompareTo(con2) < 1);
+                    return //(var1 == var2 && op2 == ExpressionType.Equal && ConstantIsGt(con2, con1)) ||
+                           (var1 == var2 && op2 == ExpressionType.LessThan && ConstantIsGte(con2, con1)) ||
+                           (var1 == var2 && op2 == ExpressionType.LessThanOrEqual && ConstantIsGte(con2, con1));
                 default: //Unsupported operation
                     return false;
             }
@@ -62,7 +62,80 @@ namespace DqMetricSimulator.Query
 
         public static bool DoesIntersect(this ISelectionCondition me, ISelectionCondition other)
         {
-            return IsSubsetOf(me, other);
+            //x cond y
+            //If equality -> x1 == x2 && y1 == y2
+            //etc.
+            var mex = me.Expression as BinaryExpression;
+            var otherx = other.Expression as BinaryExpression;
+            if (mex == null || otherx == null)
+                return false;
+            var x1 = mex.Left as ConstantExpression;
+            var y1 = mex.Right as ConstantExpression;
+            var x2 = otherx.Left as ConstantExpression;
+            var y2 = otherx.Right as ConstantExpression;
+            if ((x1 == null && y1 == null) || (x2 == null && y2 == null))
+                return false; //At least one part of the expression should be constant.
+            if ((x1 != null && y1 != null) || (x2 != null && y2 != null))
+                return false; //At least one part of the expression should be non-constant.
+            var var1 = x1 == null ? mex.Left.ToString() : mex.Right.ToString();
+            var var2 = x2 == null ? otherx.Left.ToString() : otherx.Right.ToString();
+            var con1 = x1 == null ? y1.Value : x1.Value;
+            var con2 = x2 == null ? y2.Value : x2.Value;
+            var op1 = x1 == null ? me.Expression.NodeType : Inverse(me.Expression.NodeType);
+            var op2 = x2 == null ? other.Expression.NodeType : Inverse(other.Expression.NodeType);
+
+            switch (op1)
+            {
+                case ExpressionType.Equal:
+                    return (var1 == var2 && con1 == con2) ||
+                           (var1 == var2 && op2 == ExpressionType.GreaterThan && ConstantIsGt(con1, con2)) ||
+                           (var1 == var2 && op2 == ExpressionType.GreaterThanOrEqual && ConstantIsGte(con1, con2)) ||
+                           (var1 == var2 && op2 == ExpressionType.LessThan && ConstantIsGt(con2, con1)) ||
+                           (var1 == var2 && op2 == ExpressionType.LessThanOrEqual && ConstantIsGt(con2, con1));
+                case ExpressionType.GreaterThanOrEqual:
+                    return (var1 == var2 && op2 == ExpressionType.Equal && ConstantIsGte(con1, con2)) ||
+                           (var1 == var2 && op2 == ExpressionType.GreaterThan) ||
+                           (var1 == var2 && op2 == ExpressionType.GreaterThanOrEqual) ||
+                           (var1 == var2 && op2 == ExpressionType.LessThan && ConstantIsGte(con1, con2)) ||
+                           (var1 == var2 && op2 == ExpressionType.LessThanOrEqual && ConstantIsGte(con1, con2));
+                case ExpressionType.GreaterThan:
+                    return (var1 == var2 && op2 == ExpressionType.Equal && ConstantIsGt(con1, con2)) ||
+                           (var1 == var2 && op2 == ExpressionType.GreaterThan) ||
+                           (var1 == var2 && op2 == ExpressionType.GreaterThanOrEqual) ||
+                           (var1 == var2 && op2 == ExpressionType.LessThan && ConstantIsGt(con1, con2)) ||
+                           (var1 == var2 && op2 == ExpressionType.LessThanOrEqual && ConstantIsGte(con1, con1));
+                case ExpressionType.LessThanOrEqual:
+                    return (var1 == var2 && op2 == ExpressionType.Equal && ConstantIsGte(con1, con2)) ||
+                           (var1 == var2 && op2 == ExpressionType.GreaterThan && ConstantIsGt(con1, con2)) ||
+                           (var1 == var2 && op2 == ExpressionType.GreaterThanOrEqual && ConstantIsGte(con1, con2)) ||
+                           (var1 == var2 && op2 == ExpressionType.LessThan) ||
+                           (var1 == var2 && op2 == ExpressionType.LessThanOrEqual);
+                case ExpressionType.LessThan:
+                    return (var1 == var2 && op2 == ExpressionType.Equal && ConstantIsGte(con1, con2)) ||
+                           (var1 == var2 && op2 == ExpressionType.GreaterThan && ConstantIsGt(con1, con2)) ||
+                           (var1 == var2 && op2 == ExpressionType.GreaterThanOrEqual && ConstantIsGte(con1, con2)) ||
+                           (var1 == var2 && op2 == ExpressionType.LessThan) ||
+                           (var1 == var2 && op2 == ExpressionType.LessThanOrEqual);
+                default: //Unsupported operation
+                    return false;
+            }
+        }
+
+
+        /// <summary>
+        /// Returns if con1 is greated than or equal to con2
+        /// </summary>
+        private static bool ConstantIsGte(object con1, object con2)
+        {
+            return con1 is IComparable && ((IComparable) con1).CompareTo(con2) >= 0;
+        }
+
+        /// <summary>
+        /// Returns if con1 is greated than to con2
+        /// </summary>
+        private static bool ConstantIsGt(object con1, object con2)
+        {
+            return con1 is IComparable && ((IComparable) con1).CompareTo(con2) > 0;
         }
 
         private static ExpressionType Inverse(ExpressionType expressionType)
@@ -103,9 +176,13 @@ namespace DqMetricSimulator.Query
         /// </summary>
         public static bool DoesIntersect(this IQuery me, IQuery other)
         {
-            //Any selection conditions of other does intersect with of any selection condition of me
-            //TODO: Fix this formula 
-            return other.SelectionConditions.Any(s => me.SelectionConditions.Any(mes => mes.DoesIntersect(s)));
+            //For any groupby  selection paremeter, all selection conditions of other does intersect with of any selection condition of me
+            var meCondsByPara = me.SelectionConditions.GroupBy(s => s.Parameters.FirstOrDefault().ToString()).ToList();
+            var otherCondsByPara = other.SelectionConditions.GroupBy(s => s.Parameters.FirstOrDefault().ToString()).ToList();
+
+            //Queries intersect if for all common groups, all conditions intersec
+            return meCondsByPara.Join(otherCondsByPara, g => g.Key, o => o.Key, (m, o) => new {m, o})
+                .All(j => j.m.All(s1 => j.o.Any(s2 => s1.DoesIntersect(s2))));
         }
 
         public static IMetricFunction GetMetricFunction(this IProjection me)
--- Test/TestNaiveApproach.cs
+++ Test/TestNaiveApproach.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using AlgebraToSqlServer;
 using AlgebraTree;
 using DqMetricSimulator.Core;
@@ -45,21 +46,23 @@ namespace Test
 
         [TestMethod]
         public void AddThreeQueries()
-        {
+        {   
+            //Run against AdventureWorksLite database.
             var q1 = new BasicQuery(
                 new[]
                     {
-                        ProjectionItem.CreateFromName<Int32>("Id", true),
-                        ProjectionItem.CreateFromName<String>("Name", false)
+                        ProjectionItem.CreateFromName<Int32>("ProductId", true),
+                        ProjectionItem.CreateFromName<String>("Name", false),
+                        ProjectionItem.CreateFromName<String>("Color", false)
                     },
                 new[]
                     {
-                        SelectionCondition.CreateFromLambda<string>(conferenceName => conferenceName == "VLDB")
+                        SelectionCondition.CreateFromLambda<string>(color => color == "Black")
                     },
-                new[] {"Sources"}
+                new[] {"SalesLT.Product"}
                 );
 
-            var ds = new SimpleSqlDataService();
+            var ds = new SimpleSqlDataService("Data Source=.; Initial Catalog=AdventureWorksLT2008; Integrated Security=SSPI");
             var qas = new SimpleQueryAnsweringService(ds);
             var cs = new NaiveCostService();
             var context = new SapmlingContext(qas, cs, ds);
@@ -67,7 +70,7 @@ namespace Test
 
             ITable qResult;
             var rv = context.ExecuteQuery(q1, out qResult);
-
+            
             Assert.IsNotNull(rv);
         }
 
@@ -84,7 +87,8 @@ namespace Test
                     {
                         SelectionCondition.CreateFromLambda<string>(conferenceName => conferenceName == "VLDB"),
                         SelectionCondition.CreateFromLambda<int>(year => year >= 2009),
-                        SelectionCondition.CreateFromLambda<int>(year => year <= 2009)
+                        SelectionCondition.CreateFromLambda<int>(year => year <= 2009),
+                        SelectionCondition.CreateFromLambda<int>(year => year == 2009)
                     },
                 new[] {"Sources"}
                 );
@@ -119,9 +123,58 @@ namespace Test
             Assert.IsTrue(q1.IsSubsetOf(q2));
             Assert.IsFalse(q2.IsSubsetOf(q1));
             Assert.IsTrue(q3.DoesIntersect(q2));
-            //Assert.IsTrue(q2.DoesIntersect(q3));
+            Assert.IsTrue(q2.DoesIntersect(q3));
             Assert.IsFalse(q1.DoesIntersect(q3));
             
+        }
+
+        [TestMethod]
+        public void TestSelectionConditions()
+        {
+            var s1 = SelectionCondition.CreateFromLambda<int>(year => year > 2008);
+            var s2 = SelectionCondition.CreateFromLambda<int>(year => year >= 2008);
+            var s3 = SelectionCondition.CreateFromLambda<int>(year => year < 2010);
+            var s4 = SelectionCondition.CreateFromLambda<int>(year => year <= 2010);
+            var s5 = SelectionCondition.CreateFromLambda<int>(year => year == 2009);
+            var s6 = SelectionCondition.CreateFromLambda<int>(year => year > 2009);
+            var s7 = SelectionCondition.CreateFromLambda<int>(year => year >= 2009);
+            var s8 = SelectionCondition.CreateFromLambda<int>(year => year < 2009);
+            var s9 = SelectionCondition.CreateFromLambda<int>(year => year <= 2009);
+
+            //Assert subset
+            Assert.IsTrue( s5.IsSubsetOf(s1)  );
+            Assert.IsTrue( s5.IsSubsetOf(s2)  );
+            Assert.IsTrue( s5.IsSubsetOf(s3)  );
+            Assert.IsTrue( s5.IsSubsetOf(s4)  );
+            Assert.IsFalse( s1.IsSubsetOf(s5) );
+            Assert.IsTrue( s1.IsSubsetOf(s2) );
+            Assert.IsFalse( s2.IsSubsetOf(s1) );
+            Assert.IsTrue( s6.IsSubsetOf(s1));
+            Assert.IsTrue( s7.IsSubsetOf(s1));
+            Assert.IsTrue(s6.IsSubsetOf(s2));
+            Assert.IsTrue(s7.IsSubsetOf(s2));
+            Assert.IsTrue(s8.IsSubsetOf(s3));
+            Assert.IsTrue(s9.IsSubsetOf(s3));
+            Assert.IsTrue(s8.IsSubsetOf(s4));
+            Assert.IsTrue(s9.IsSubsetOf(s4));
+
+            //Assert intersect
+            Assert.IsTrue(s1.DoesIntersect(s2));
+            Assert.IsTrue(s2.DoesIntersect(s1));
+            Assert.IsTrue(s3.DoesIntersect(s1));
+            Assert.IsTrue(s4.DoesIntersect(s1));
+            Assert.IsTrue(s3.DoesIntersect(s2));
+            Assert.IsTrue(s4.DoesIntersect(s2));
+            Assert.IsTrue(s5.DoesIntersect(s1));
+            Assert.IsTrue(s5.DoesIntersect(s2));
+            Assert.IsTrue(s5.DoesIntersect(s3));
+            Assert.IsTrue(s5.DoesIntersect(s4));
+            Assert.IsTrue(s5.DoesIntersect(s1));
+            Assert.IsTrue(s2.DoesIntersect(s7));
+            Assert.IsTrue(s7.DoesIntersect(s9));
+            Assert.IsTrue(s9.DoesIntersect(s7));
+            Assert.IsFalse(s6.DoesIntersect(s8));
+            Assert.IsFalse(s8.DoesIntersect(s6));
         }
     }
 }
